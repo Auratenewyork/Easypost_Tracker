d1bfd3a9942f0e9069747a02ebc25199348edee6
Nazar Kaluzghnyy <kaluzghnyy@gmail.com>
2021-02-19 12:00:19 +0200

    Add key

--- src/components/multiple-arrivals/ArrivalItem.js
+++ src/components/multiple-arrivals/ArrivalItem.js
@@ -10,8 +10,7 @@ const ArrivalItem = ({ onClick, ...item }) => {
     return (
         <div className="arrival-item-wrapper" onClick={handleClick}>
             {item.products.map(product => (
-
-                <div className="arrival-item">
+                <div key={product.name} className="arrival-item">
                     <img className="arrival-item__image" src={product.image} alt={product.name} />
                     <div className="arrival-item__info">
                         <span style={{ marginBottom: 10 }}>{product.name}</span>
