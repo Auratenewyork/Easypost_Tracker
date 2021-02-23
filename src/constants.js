1614e284cdba61d836a32e0254608c5d60337257
Nazar Kaluzghnyy <kaluzghnyy@gmail.com>
2021-02-23 18:27:46 +0200

    Configure dev env

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export const API_LINK = process.env.NODE_ENV === 'production' ? 'https://4p9vek36rc.execute-api.us-east-2.amazonaws.com/api/tracking_information' : 'https://4p9vek36rc.execute-api.us-east-2.amazonaws.com/api/tracking_information_'
+export const API_LINK = process.env.REACT_APP_ENV === 'production' ? 'https://4p9vek36rc.execute-api.us-east-2.amazonaws.com/api/tracking_information' : 'https://4p9vek36rc.execute-api.us-east-2.amazonaws.com/api/tracking_information_'
